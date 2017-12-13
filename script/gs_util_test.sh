7ea8f61370b633625c0c8c9bfd9de37bfe41cd6f
anrewkaka <mr.anrew@gmail.com>
2017-12-13 15:28:26 +0700

    Update gs_util_test.sh

--- script/gs_util_test.sh
+++ script/gs_util_test.sh
@@ -39,7 +39,7 @@ fi
 for GCS_DELETE_FOLDER in ${GCS_DELETE_FOLDERS}; do
     GCS_FOLDER_NAME=`basename ${GCS_DELETE_FOLDER}`
     echo ${GCS_FOLDER_NAME}
-    GCS_FOLDER_CREATED_DATE=`cut -d '_' -f 2- <<< ${GCS_FOLDER_NAME}`
+    GCS_FOLDER_CREATED_DATE=`cut -d'_' -f 2- <<< ${GCS_FOLDER_NAME}`
     echo ${GCS_FOLDER_CREATED_DATE}
 done
 

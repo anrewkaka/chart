976150dc91618be9f2a2ee6d6cd9665af561d253
anrewkaka <mr.anrew@gmail.com>
2017-12-13 14:38:36 +0700

    Update gs_util_test.sh

--- script/gs_util_test.sh
+++ script/gs_util_test.sh
@@ -38,7 +38,7 @@ fi
 # for GCS_DELETE_FOLDER in `gsutil ls -d gs://${DATA_BUCKET}/delete_folder/*${TARGET_LOAD_GROUPID}`; do
 for GCS_DELETE_FOLDER in ${GCS_DELETE_FOLDERS}; do
     GCS_FOLDER_NAME=`basename ${GCS_DELETE_FOLDER}`
-    GCS_FOLDER_CREATED_DATE=`$(cut -d'-' -f1 <<<${GCS_FOLDER_NAME})`
+    IFS=';' read -r GCS_FOLDER_CREATED_DATE <<< ${GCS_FOLDER_NAME}
     echo ${GCS_FOLDER_CREATED_DATE}
     echo ${GCS_FOLDER_NAME}
 done

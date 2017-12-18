e8f76fc7aab005ff1a9505b5072d435f91d730b6
anrewkaka <mr.anrew@gmail.com>
2017-12-18 17:26:03 +0700

    Update del.sh

--- script/del.sh
+++ script/del.sh
@@ -62,10 +62,10 @@ delete_by_path() {
 
 unset GCS_DELETE_DIRECTORIES[0]
 
-echo ${GCS_DELETE_DIRECTORIES}
-echo ${#GCS_DELETE_DIRECTORIES}
+echo $(expr ${GCS_DELETE_DIRECTORIES} \== ${GCS_TARGET_PATH})
+
 exit 0
-    if [ ${#GCS_DELETE_DIRECTORIES[@]} -eq 0 ]; then
+    if [ $(expr ${GCS_DELETE_DIRECTORIES} \== ${GCS_TARGET_PATH}) -eq 0 ]; then
         # ログ出力
         "`date '+%T'` 空のディレクトリ：" ${GCS_TARGET_PATH} | tee -a ${GCS_SEND_LOG}
     fi

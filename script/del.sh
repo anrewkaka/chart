6e8d59ff85dce96d0defe69bfc760d0063cc6516
anrewkaka <mr.anrew@gmail.com>
2017-12-18 18:02:34 +0700

    Update del.sh

--- script/del.sh
+++ script/del.sh
@@ -54,7 +54,7 @@ gcloud config set project ${PROJECT_NAME}
 delete_by_path() {
     GCS_TARGET_DIRECTORY=$1
     GCS_TARGET_PATH=`echo gs://${DATA_BUCKET}/${GCS_TARGET_DIRECTORY}/`
-    GCS_DELETE_DIRECTORIES=`gsutil ls -r ${GCS_TARGET_PATH}`
+    GCS_DELETE_DIRECTORIES=`gsutil ls ${GCS_TARGET_PATH}**`
     if [ ${RETURN_CD} -ne 0 ]; then
         # 異常終了
         exit 1

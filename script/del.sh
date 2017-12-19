6cb386eab8d95a4961fd339dbe1bc355909cab7a
anrewkaka <mr.anrew@gmail.com>
2017-12-19 17:24:04 +0700

    Update del.sh

--- script/del.sh
+++ script/del.sh
@@ -70,8 +70,7 @@ delete_by_path() {
         GCS_DELETE_DIRECTORY_NAME=`basename ${GCS_DELETE_DIRECTORY}`
         GCS_DIRECTORY_CREATED_DATE=${GCS_DELETE_DIRECTORY_NAME%%_*}
 
-        if [ $(expr ${GCS_DIRECTORY_CREATED_DATE} : "^[0-9]\{8\}$") != 8 ]; then
-            echo "x`$(expr ${GCS_DIRECTORY_CREATED_DATE} : "^[0-9]`{8`}$")`"
+        if [ $(expr "$GCS_DIRECTORY_CREATED_DATE" : '[0-9]\{8\}') != 8 ]; then
             echo ${GCS_DELETE_DIRECTORY}
             continue
         fi
